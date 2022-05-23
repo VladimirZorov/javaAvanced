0b153e1ae3c091854d710e8d5d889231756060b6
Vladimir Zorov <96238684+VladimirZorov@users.noreply.github.com>
2022-05-23 22:43:28 +0300

    Add files via upload

---
+++ FilltheMatrix.java
@@ -0,0 +1,10 @@
+package MultidimensionalArraysExercises;
+
+import java.util.Scanner;
+
+public class FilltheMatrix {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+    }
+}
